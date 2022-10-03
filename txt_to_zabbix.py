935578b8a80cdfccf4cdd9944f1287b6c6205747
onspeed83 <tetyaasya@mail.ru>
2022-10-03 17:26:32 +0300

    Update txt_to_zabbix.py
    Editing the section for selecting the correct interface for the new host being created.

--- txt_to_zabbix.py
+++ txt_to_zabbix.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python3
-#Simple and ugly script for adding hosts to zabbix from text file using zabbix api
-# written for zabbix 4.2 
-
 import sys
 from sys import argv
 import requests
 import json
+import urllib3
+
+urllib3.disable_warnings()
 
 srv_group = '7' #required parameter
 srv_templ_id = '10278'  #required parameter template ID  (in my case - Hepervisors)
 create_enabled = 'false' # adding hosts disabled or enabled
-zabbix_username = 'zabbix_username'
-zabbix_password = 'zabbix_password'
-zabbix_url = 'http://zabbix_server_ip/api_jsonrpc.php'  
+zabbix_username = 'user'
+zabbix_password = 'pass'
+zabbix_url = 'LINK/api_jsonrpc.php'
 headers = {'content-type': 'application/json'}  #leave as-is
 
 
-filename = str(sys.argv[1])  
+filename = str(sys.argv[1])
 
 f = open(filename)
-f_input = f.read().split('\n') 
+f_input = f.read().split('\n')
 f.close()
 del f_input[0] #remove 1st string from file. Comment it, if you want to use first string (without description)
 
@@ -28,32 +27,31 @@ s = requests.Session()
 
 auth_payload = {'jsonrpc': '2.0','method':'user.login','params':{'user':zabbix_username,'password':zabbix_password},'id':'1'}
 
-r_auth = s.post(zabbix_url, data=json.dumps(auth_payload), headers=headers, verify=False,)
+r_auth = s.post(zabbix_url, data=json.dumps(auth_payload), headers=headers, verify=False)
 result=r_auth.json()
+
+#print(result)
+
+
 auth_key=result['result'] # getting authorization key  from zabbix
 
 
 
 for i in f_input:
 
-    string_res = i.split();  
-    
+    string_res = i.split();
+
 
     srv_name = string_res[0]
     srv_type = string_res[1]
     srv_ip = string_res[2]
     srv_port = string_res[3]
-    
+
     #choosing tipe of host's interface
     if string_res[1] == 'Agent':
-        interface_type = '1'        
-    elif string_res[1] == 'SNMP':
-        interface_type = '2' # SNMP
-    else:
-         print('No interface type defined!')
-         sys.exit()
+        interface_type = '1'
 
-    create_payload = {
+        create_payload = {
             "jsonrpc": "2.0",
             "method": "host.create",
             "params": {
@@ -83,10 +81,54 @@ for i in f_input:
             "id": 1
         }
 
-    r_create = s.post(zabbix_url, data=json.dumps(create_payload), headers=headers, verify=False,)
-    
+    elif string_res[1] == 'SNMP':
+        interface_type = '2' # SNMP
+
+        create_payload = {
+            "jsonrpc": "2.0",
+            "method": "host.create",
+            "params": {
+                "host": srv_name,
+                "interfaces": [
+                    {
+                        "type": interface_type,
+                        "main": 1,
+                        "useip": 1,
+                        "ip": srv_ip,
+                        "dns": "",
+                        "port": srv_port,
+                        "details": {
+                            "version": 2,
+                            "bulk": 1,
+                            "community": "{$SNMP_COMMUNITY}"
+                        }
+                    }
+                ],
+                "groups": [
+                    {
+                        "groupid": srv_group
+                    }
+                ],
+                "templates": [
+                    {
+                        "templateid": srv_templ_id
+                    }
+                ],
+            },
+            "auth": auth_key,
+            "id": 1
+        }
+
+    else:
+         print('No interface type defined!')
+         sys.exit()
+
+
+    #print(json.dumps(create_payload))
+    r_create = s.post(zabbix_url, data=json.dumps(create_payload), headers=headers, verify=False)
+
     #print(r_create.text)
-    
+
     result = r_create.json()['result']
     host_ids = result['hostids'][0]
     print('Host created with ID: ', host_ids)  #creation result
@@ -102,6 +144,6 @@ for i in f_input:
         sys.exit()
 
     upayload = {"jsonrpc": "2.0","method": "host.update","params": {"hostid": host_ids,"status": create_status},"auth": auth_key,"id": 1}
-    r_upd = s.post(zabbix_url, data=json.dumps(upayload), headers=headers, verify=False,)
+    r_upd = s.post(zabbix_url, data=json.dumps(upayload), headers=headers, verify=False)
 
 requests.Session().close()
